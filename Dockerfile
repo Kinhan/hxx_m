04404779070fbecb1cc09512d5dd14f1ab0649e0
linjinhan <linjinhan@gt.cn>
2022-05-15 21:52:34 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM alpine:edge
+COPY xx.zip /xx/xx.zip
+COPY caddy/static-html /usr/share/caddy/
+COPY caddy/h5-speedtest /usr/share/caddy/speedtest/
+COPY caddy/Caddyfile /etc/caddy/CaddyfileTemp
+#COPY nginx/default.conf.template /etc/nginx/conf.d/default.conf.template
+#COPY nginx/nginx.conf /etc/nginx/nginx.conf
+COPY configure.sh /configure.sh
+COPY xx_config /
+RUN apk update && \
+    apk add --no-cache ca-certificates caddy tor wget ca-certificates bash curl php7 && \
+    unzip /xx/xx.zip -d /xx/ && \
+    chmod +x /xx/xx && \
+    chmod +x /xx/v2ctl && \
+    rm -rf /var/cache/apk/*
+
+RUN chmod +x /configure.sh
+CMD sed -e "s/\$UUID/$UUID/g" /xx.json > /xx/config.json && sed -e "1c :$PORT" /etc/caddy/CaddyfileTemp > /etc/caddy/Caddyfile && /configure.sh
---
+++ configure.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# xx generate configuration
+# Download and install xx
+#config_path="/xx.json"
+#install -m 755 /xx/xx /usr/local/bin/xx
+#install -m 755 /xx/v2ctl /usr/local/bin/v2ctl
+# Remove temporary directory
+#rm -rf /xx
+# xx new configuration
+#install -d /usr/local/etc/xx
+#envsubst '\$UUID' < $config_path > /xx/xxtemp.json
+#/bin/bash -c "envsubst '\$UUID' < /xx.json > /xx/config.json"
+#sed -e "s/\$UUID/$UUID/g" /xx.json > /xx/config.json
+# MK TEST FILES
+mkdir /opt/test
+cd /opt/test
+dd if=/dev/zero of=100mb.bin bs=100M count=1
+dd if=/dev/zero of=10mb.bin bs=10M count=1
+# Run xx
+/xx/xx -config /xx/config.json &
+
+## Run caddy
+#mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "" > /usr/share/caddy/robots.txt
+#sed -i "1c :$PORT" /etc/caddy/CaddyfileTemp
+#/bin/bash -c `sed -i "1c :$PORT" > /etc/caddy/Caddyfile`
+
+# run caddy
+tor &
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
